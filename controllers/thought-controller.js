df429da82ec7f10e1103e4937283b1420802cf53
jake <jakedlee2018@gmail.com>
2021-10-06 18:51:53 -0500

    controllers progress

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,33 +1,69 @@
 const { Thought } = require('../models');
 
 const thoughtController = {
-    getAllThoughts(req, res) {
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch( err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+  },
 
-    },
+  getThoughtByID({params}, res) {
+    Thought.findOne({ _id: params.id })
+    .then(dbThoughtData => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: 'Thought unable to be retreived' });
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+  },
 
-    getThoughtByID({params}, res) {
+  createNewThought({body}, res) {
+    Thought.create(body)
+      .then(dbUserData => 
+        res.json(dbUserData))
+      .catch(err => 
+        res.status(400).json(err));
+  },
 
-    },
+  updateThought({params, body}, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, {new:true})
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ mesage:'Thought could not be translated'});
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
 
-    createNewThought({body}, res) {
+  deleteThought({params}, res) {
+    Thought.findOneAndDelete({ _id: params.id }, body, {new:true})
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ mesage:'Cannot fetch the current thought'});
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
 
-    },
+  addReaction({params}, res) {
 
-    updateThought({params, body}, res) {
+  },
 
-    },
+  removeFriend({params}, res) {
 
-    deleteThought({params}, res) {
-
-    },
-
-    addReaction({params}, res) {
-
-    },
-
-    removeFriend({params}, res) {
-
-    }
+  }
 }
 
 module.exports = thoughtController;
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,27 +2,71 @@ const { User } = require('../models');
 
 const userController = {
   getAllUser(req, res) {
-        
+    User.find({})
+      .then(dbUserData => res.json(dbUserData))
+      .catch( err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
   },
 
   getUserByID({params}, res) {
-
+    User.findOne({ _id: params.id })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'User not found' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
   },
 
   createNewUser({body}, res) {
-
+    User.Create(body)
+      .then(dbUserData => 
+        res.json(dbUserData))
+      .catch(err => 
+        res.status(400).json(err));
   },
 
   updateUser({params, body}, res) {
-
+    User.findOneAndUpdate({ _id: params.id }, body, {new: true})
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ mesage:'Unable to find User'});
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
   },
 
   deleteUser({params}, res) {
-
+    User.findOneAndDelete({ _id: params.id })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message:'Sorry User does not exist'});
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => res.status(400).json(err));
   },
 
-  addFriend({params}, res) {
-
+  addFriend({params, body}, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {new: true})
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ mesage:'Wrong User'});
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
   },
 
   removeFriend({params}, res) {
